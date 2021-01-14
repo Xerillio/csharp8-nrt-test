7be7888f9f2137275e47fc72e77bf4c31bc28632
Casper Sørensen <cas0493@gmail.com>
2021-01-14 12:52:48 +0100

    Initial commit

---
+++ LegacyLibrary/PersonFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LegacyLibrary
+{
+    public static class PersonFactory
+    {
+        public static Person CreatePerson(string firstName, string lastName, int age)
+        {
+            if (!string.IsNullOrWhiteSpace(firstName)
+                && !string.IsNullOrWhiteSpace(lastName)
+                && age >= 0)
+            {
+                return new Person { FirstName = firstName, LastName = lastName, Age = age };
+            }
+            return null;
+        }
+    }
+}
---
+++ NrtEnabledProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using LegacyLibrary;
+using System;
+
+namespace NrtEnabledProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userInfo = GetUserInfo();
+
+            Console.WriteLine("Your name is {0} {1} and you are {2} years old",
+                userInfo.FirstName, userInfo.LastName, userInfo.Age);
+        }
+
+        private static Person GetUserInfo()
+        {
+            Console.WriteLine("Please enter your first name:");
+            var firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name:");
+            var lastName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your age:");
+            int.TryParse(Console.ReadLine(), out var age);
+
+            var person = PersonFactory.CreatePerson(firstName, lastName, age);
+            return person;
+        }
+    }
+}
